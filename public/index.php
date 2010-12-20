4d6c9e8dad8589cd7cd652ad7a43807edaee1117
Sascha-Oliver Prolic <sprolic@frogster.de>
2010-12-20 15:52:59 +0100

    fixed typo

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ defined('APPLICATION_PATH')
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-die(APPLICATION_ENV);
+
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
